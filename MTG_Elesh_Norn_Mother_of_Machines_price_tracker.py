6304820a3291dd3e623aeb24fa028854e38e7a2e
Carl-Fredrik Rif <carl-fredrik.rif.data24hel@edu.tucsweden.se>
2024-12-18 15:55:03 +0100

    Removed Placeholder url and moved API fetiching to dedicated function

--- MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
+++ MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
@@ -2,6 +2,7 @@ import requests
 import json
 import csv
 import logging
+import time
 
 logging.basicConfig(
     filename= 'logfile.log',
@@ -11,18 +12,25 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-url = 'http://httpbin.org/headers' #placeholder url for debugging purposes
-user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.1'}
+#url = 'https://api.scryfall.com/cards/44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
+base_url = 'https://api.scryfall.com/cards'
+user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.2'}
 
-#logger.info('Sum of %d and %d is %d', i, j, mult_sum)
-#TODO: Bake the JSON fetching into a function
-logger.info('Sending REST Request to %s using User-Agent %s', url, user_agent_name)
-response = requests.get(url, headers=user_agent_name)
+def scryfall_card_fetch(mtgcard):
+    url = f"{base_url}/{mtgcard}"
+    time.sleep(0.1) #Scryfall requests a 50-100 milisecond wait between responses, this is here to ensure that
+    logger.info('Sending REST Request to %s using User-Agent %s', url, user_agent_name)
+    response = requests.get(url, headers=user_agent_name, verify=True)
+    return response
 
-if response.status_code == '200':
+response = scryfall_card_fetch('44dcab01-1d13-4dfc-ae2f-fbaa3dd35087')
+
+if response.status_code == 200:
     logger.info('Card info sucessfully retrieved, HTTP Code: %d', response.status_code)
 else:
     logger.critical('FATAL ERROR: Card not retrieved, HTTP Code: %d', response.status_code)
 
 print(response.status_code) #Temporary code for debugging purposes
 print(response.text)
+
+#logger.info('Sum of %d and %d is %d', i, j, mult_sum)
