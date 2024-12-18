8a2419686406d11ac016bd33c81189c5dc2ff433
Carl-Fredrik Rif <carl-fredrik.rif.data24hel@edu.tucsweden.se>
2024-12-18 21:58:11 +0100

    started on the write to CSV function, still needs fixing

--- MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
+++ MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
@@ -4,6 +4,8 @@ import csv
 import logging
 import time
 import sys
+import os
+from datetime import date
 
 logging.basicConfig(
     filename= 'logfile.log',
@@ -15,7 +17,7 @@ logger.setLevel(logging.DEBUG)
 
 #url = 'https://api.scryfall.com/cards/44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
 base_url = 'https://api.scryfall.com/cards'
-user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.2'}
+user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.3'} #scryfall requires the user-agent to be named after its specific purpose
 
 def scryfall_card_fetch(mtgcard):
     url = f"{base_url}/{mtgcard}"
@@ -23,8 +25,8 @@ def scryfall_card_fetch(mtgcard):
     logger.debug('Sending REST Request to %s using User-Agent %s', url, user_agent_name)
     response = requests.get(url, headers=user_agent_name, verify=True) #scryfall requires API requests to be handled through HTTPS
     if response.status_code == 200:
-        logger.info('Card info sucessfully retrieved, HTTP Code: %d', response.status_code)
         card_info = response.json()
+        logger.info('MTG Card: %s sucessfully retrieved, HTTP Code: %d',card_info['name'], response.status_code)
         return card_info
     else:
         logger.critical('FATAL ERROR: Card not retrieved, HTTP Code: %d', response.status_code)
@@ -32,5 +34,24 @@ def scryfall_card_fetch(mtgcard):
 
 card_id = '44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
 card_info = scryfall_card_fetch(card_id)
+print(card_info['prices'])
+print(card_info['name'])
 
+csv_mode = ''
+if os.path.exists('Elesh_Norn_Price_History.csv'):
+    csv_mode = 'a'
+else:
+    csv_mode ='w'
+#TODO: fix the append part of the csv writing script
+with open('Elesh_Norn_Price_History.csv', mode=csv_mode) as csvfile:
+    fieldnames = ['name', 'usd', 'usd_foil', 'usd_etched', 'eur', 'eur_foil', 'tix', 'date_retrieved']
+    csv_dictionary = {}
+    csv_dictionary['name'] = card_info['name']
+    price_info = card_info['prices']
+    for price_type in price_info:
+        csv_dictionary[price_type] = price_info[price_type]
+    csv_dictionary['date_retrieved'] = date.today()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow(csv_dictionary)
 #logger.info('Sum of %d and %d is %d', i, j, mult_sum)
