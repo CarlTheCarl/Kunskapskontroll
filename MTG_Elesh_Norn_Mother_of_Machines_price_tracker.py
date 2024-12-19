5e37241faae7853e5ee0a77f6e84dfc95accb3d7
Carl-Fredrik Rif <carl-fredrik.rif.data24hel@edu.tucsweden.se>
2024-12-19 11:49:53 +0100

    csv function now writes item to multiple cells instead of the same one

--- MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
+++ MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
@@ -17,7 +17,7 @@ logger.setLevel(logging.DEBUG)
 
 #url = 'https://api.scryfall.com/cards/44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
 base_url = 'https://api.scryfall.com/cards'
-user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.3'} #scryfall requires the user-agent to be named after its specific purpose
+user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/0.4'} #scryfall requires the user-agent to be named after its specific purpose
 
 def scryfall_card_fetch(mtgcard):
     url = f"{base_url}/{mtgcard}"
@@ -29,39 +29,29 @@ def scryfall_card_fetch(mtgcard):
         logger.info('MTG Card: %s sucessfully retrieved, HTTP Code: %d',card_info['name'], response.status_code)
         return card_info
     else:
-        logger.critical('FATAL ERROR: Card not retrieved, HTTP Code: %d', response.status_code)
+        logger.critical('FATAL ERROR: Card could not be retrieved, HTTP Code: %d', response.status_code)
         sys.exit('Card Fetch Failed HTTP Code: %d', response.status_code)
 
-card_id = '44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
+card_id = '44dcab01-1d13-4dfc-ae2f-fbaa3dd35087' #scryfall allows for multiple search type, i went with id because it felt the most secure
 card_info = scryfall_card_fetch(card_id)
-print(card_info['prices'])
-print(card_info['name'])
 
-# csv_dictionary = {}
-# csv_dictionary['name'] = card_info['name']
-# price_info = card_info['prices']
-# for price_type in price_info:
-#     csv_dictionary[price_type] = price_info[price_type]
-# csv_dictionary['date_retrieved'] = date.today()
-
-csv_list = []
-csv_list.append(card_info['name'])
+csv_dictionary = {}
+csv_dictionary['name'] = card_info['name']
 price_info = card_info['prices']
 for price_type in price_info:
-    print(price_info[price_type])
-    csv_list.append(price_info[price_type])
-csv_list.append(date.today())
+    csv_dictionary[price_type] = price_info[price_type]
+csv_dictionary['date_retrieved'] = date.today()
 
+fieldnames = ['name', 'usd', 'usd_foil', 'usd_etched', 'eur', 'eur_foil', 'tix', 'date_retrieved']
+#TODO: fix so that all the diffrent headers and fields are in seperate cells
 if os.path.exists('Elesh_Norn_Price_History.csv'):
     with open('Elesh_Norn_Price_History.csv', mode='a') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(csv_list)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        writer.writerow(csv_dictionary)
 else:
     with open('Elesh_Norn_Price_History.csv', mode='w') as csvfile:
-        fieldnames = ['name', 'usd', 'usd_foil', 'usd_etched', 'eur', 'eur_foil', 'tix', 'date_retrieved']
-        writer = csv.writer(csvfile, fieldnames=fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
         writer.writeheader()
-        writer.writerow(csv_list)
-#TODO: fix the append part of the csv writing script
+        writer.writerow(csv_dictionary)
 
 #logger.info('Sum of %d and %d is %d', i, j, mult_sum)
