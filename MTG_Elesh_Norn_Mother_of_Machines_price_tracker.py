356caf43a2c8dbc0d21af4ee7eefe10904691579
Carl-Fredrik Rif <carl-fredrik.rif.data24hel@edu.tucsweden.se>
2024-12-30 09:59:42 +0100

    Removed unused import and commeted code

--- MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
+++ MTG_Elesh_Norn_Mother_of_Machines_price_tracker.py
@@ -1,6 +1,4 @@
 import requests
-import json
-import csv
 import logging
 import time
 import sys
@@ -16,9 +14,8 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-#url = 'https://api.scryfall.com/cards/44dcab01-1d13-4dfc-ae2f-fbaa3dd35087'
 base_url = 'https://api.scryfall.com/cards'
-user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/1.0'} #scryfall requires the user-agent to be named after its specific purpose
+user_agent_name = {'User-Agent':'MTG Elesh Norn Mother of Machines Price Tracker/1.0.1'} #scryfall requires the user-agent to be named after its specific purpose
 
 def scryfall_card_fetch(mtgcard):
     url = f"{base_url}/{mtgcard}"
@@ -47,15 +44,6 @@ for price_type in price_info:
 csv_dictionary['date_retrieved'] = date.today()
 
 print(csv_dictionary)
-# csv_list = []
-# csv_list.append(card_info['name'])
-# price_info = card_info['prices']
-# for price_type in price_info:
-#     if price_info[price_type] != None:
-#         csv_list.append(float(price_info[price_type]))
-#     else:
-#         csv_list.append(price_info[price_type])
-# csv_list.append(date.today())
 
 fieldnames = ['name', 'usd', 'usd_foil', 'usd_etched', 'eur', 'eur_foil', 'tix', 'date_retrieved']
 #TODO: fix so that float works in excel
@@ -65,15 +53,3 @@ if os.path.exists('Elesh_Norn_Price_History.csv'):
     df.to_csv('Elesh_Norn_Price_History.csv',mode='a', header=False, sep=';', decimal =',', index=False)
 else:
     df.to_csv('Elesh_Norn_Price_History.csv', header=fieldnames, sep=';', decimal =',', index=False)
-    
-# if os.path.exists('Elesh_Norn_Price_History.csv'):
-#     with open('Elesh_Norn_Price_History.csv', mode='a') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';', decmial = ',')
-#         writer.writerow(csv_dictionary)
-# else:
-#     with open('Elesh_Norn_Price_History.csv', mode='w') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';', decmial = ',')
-#         writer.writeheader()
-#         writer.writerow(csv_dictionary)
-
-#logger.info('Sum of %d and %d is %d', i, j, mult_sum)
